c5ff65a9b415b441809a2685020ea61ed8d9beb9
phuonguyen11 <phuonguyen11112002@gmail.com>
2023-11-10 23:13:38 +0700

    add api

--- src/api/index.js
+++ src/api/index.js
@@ -4,8 +4,18 @@ import axios from "axios";
 //call api from backend
 export const getProject = async () => {
 	const response = await axios.get(
-		`http://localhost:3000/project`
+		`http://localhost:7789/project`
 	);
      return response.data;
 };
 
+export const deleteProject = async (projectId) => {
+	try {
+		const response = await axios.delete(`http://localhost:7789/project/${projectId}`);
+		console.log(response.data.message); // Log the success message
+	} catch (error) {
+	  console.error('Error deleting project:', error);
+	  throw error;
+	}
+  };
+  
