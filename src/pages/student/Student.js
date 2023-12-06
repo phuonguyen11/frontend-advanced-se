247a1bd9d0f6db190fc7356dee3e6e29ad5d2a8c
phuonguyen11 <phuonguyen11112002@gmail.com>
2023-12-07 01:26:51 +0700

    student application

--- src/pages/student/Student.js
+++ src/pages/student/Student.js
@@ -1,40 +1,14 @@
 import React, { useState } from "react";
 import './styles.css';
 import { useEffect } from "react";
-import { getAllProjectsOfAllUnis, updateProjectChecked, getUni } from "../../api";
+import { getAllProjectsOfAllUnis, updateProjectChecked, updateApplyProject, getUni } from "../../api";
+import { Typography } from "@material-ui/core";
+import { getProject } from "../../api";
 
 export default function Student() {
   const [projects, setProjectData] = useState([]);
   const [unis, setUnis] = useState([]);
-  const uniOfStaff = localStorage.getItem("uni_id");
-
-
-  const handleAccept = async (projectId) => {
-    const res = await updateProjectChecked(projectId, true);
-    console.log(res);
-    console.log("ok");
-    await getProjectData();
-  };
-
-  const handleReject = async (projectId) => {
-    const res = await updateProjectChecked(projectId, false);
-    await getProjectData();
-    console.log(res);
-    console.log("ok");
-  };
-
-  async function getProjectData() {
-    try {
-      const result = await getAllProjectsOfAllUnis();
-      if (result !== undefined) {
-        setProjectData(result);
-      }
-
-    } catch (error) {
-      console.error("Error fetching project data", error);
-      throw error; // Re-throw the error to handle it outside if needed
-    }
-  }
+  const [projectApplied, setProjectApplied] = useState([]);
 
   async function getUniData() {
     try {
@@ -49,37 +23,42 @@ export default function Student() {
     }
   }
 
-  const getAllData = async () => {
-    await Promise.all([getProjectData(), getUniData()])
-  }
-
-  useEffect(() => {
-
-    getAllData();
-
-  }, []);
-
+  const handleAccept = async (projectId) => {
+    const res = await updateApplyProject(projectId);
+    console.log(res);
+    await getProjectData();
+  };
   const findUniOfProject = (project) => {
-
     const uni = unis.find(uniData => uniData.id === project.uni_id);
     console.log({ unis })
     return uni;
-
   }
 
-  const getFilteredProject = () => {
-    console.log(uniOfStaff);
-    return projects.filter((project) => {
-      return Number(project.uni_id) === Number(uniOfStaff);
-    });
+  async function getProjectData() {
+    try {
+      const result = await getAllProjectsOfAllUnis();
+      const appliedProjects = await getProject();
+      const appliedProjectIds = appliedProjects.map(project => project.id);
+      setProjectApplied(appliedProjectIds);
+      setProjectData(result);
+    } catch (error) {
+      console.error("Error fetching project data", error);
+      throw error; // Re-throw the error to handle it outside if needed
+    }
   }
+  const getAllData = async () => {
+    await Promise.all([getProjectData(), getUniData()])
+  }
+  useEffect(() => {
+    getAllData();
+  }, []);
 
   return (
     <>
     {localStorage.getItem("role") === "0" ? (
     <>
       <div>
-        <h1>University</h1>
+        <h1>Student Board</h1>
         <h2>Project List</h2>
         <table className="project-table">
           <thead>
@@ -92,37 +71,28 @@ export default function Student() {
               <th style={{ width: '5%' }}>Quantity</th>
               <th style={{ width: '8%' }}>Start Date</th>
               <th style={{ width: '8%' }}>End Date</th>
-              <th>Status</th>
               <th style={{ width: '25%' }}>Action</th>
             </tr>
           </thead>
           <tbody>
-            {getFilteredProject().map((project, index) => {
+            {projects.map((project, index) => {
               const uni = findUniOfProject(project)
-              // console.log('project', project, uni)
               return (
                 <tr key={project.id}>
                   <td>{index + 1}</td>
                   <td>{project.name}</td>
-                  {/* <td>{project.uni_id}</td> */}
                   <td>{uni ? uni.name : ""}</td>
                   <td>{project.description}</td>
                   <td>{project.location}</td>
                   <td>{project.quantity}</td>
                   <td>{new Date(project.start_date).toLocaleDateString()}</td>
                   <td>{new Date(project.end_date).toLocaleDateString()}</td>
-                  <td>{project.is_checked == null ? "Pending" : project.is_checked == true ? "Accepted" : "Rejected"}</td>
                   <td>
-                    {project.is_checked == null ?
-                      <>
-                        <button disabled={project.is_checked != null} className="accept-btn" onClick={() => handleAccept(project.id)}>
-                          Accept
+                    {project.is_checked === true && !projectApplied.includes(project.id)?
+                        <button className="accept-btn" onClick={() => handleAccept(project.id)}>
+                          Apply
                         </button>
-                        <button disabled={project.is_checked != null} className="reject-btn" onClick={() => handleReject(project.id)}>
-                          Reject
-                        </button>
-                      </>
-                      : ""}
+                    :<Typography style={{color:"red"}}>Applied</Typography>}
                   </td>
                 </tr>
               )
